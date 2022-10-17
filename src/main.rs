f62f0699713f722fa53f60c64dcc744c486306b3
linknmike <mikeheidal@gmail.com>
2022-10-16 22:26:42 -0700

    Removed if preventing whole project from running...

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ mod day02;
 mod day03;
 
 fn main() {
-    if false {
-        day01::solve_puzzle();
-        day02::solve_puzzle();
-        day03::solve_puzzle();
-    }
+    day01::solve_puzzle();
+    day02::solve_puzzle();
+    day03::solve_puzzle();
 }
